5f13b72ec2439db0d0db8e45ee7d06bfe6538625
Brodys1 <123720584+Brodys1@users.noreply.github.com>
2023-03-06 14:01:15 -0800

    Q4

--- q2.py
+++ q2.py
@@ -1,6 +1,19 @@
 
 # ******************************
 # Make your Code
+longest = 0
+shortest = 100
+while True:
+    ustr = input('input a string: ')
+    if ustr == 'stop' or ustr == 'STOP':
+        break
+    if len(ustr) > longest:
+        longest = len(ustr)
+        longword = ustr
+    if len(ustr) < shortest:
+        shortest = len(ustr)
+        shortword = ustr
+print(f'Longest word: {longword}, Shortest word: {shortword}')
 # ******************************
 
 
